9f36ba8fba9ca53b85c400121a7663ca9cbbdda3
1991artem <ar_puz@mail.ru>
2022-11-29 21:54:14 +0300

    fix: fix token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,9 @@
-import { Controller, Post, Body, UseGuards, Res } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from 'src/user/dto/create-user.dto';
 import { AuthService } from './auth.service';
 import { LoginUserDto } from './dto/login-user.dto';
-import { Cookies } from 'src/decorators/cookie.decorator';
-import { JwtAuthGuard } from './jwt-token.guard';
+
 
 @ApiTags('Authorization')
 @Controller('auth')
